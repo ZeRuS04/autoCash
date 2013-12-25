e0fccd1310d72ebf0b47fc5f1251f9aaaf11256d
ZeRuS04 <zexa05@yandex.ru>
2013-12-25 05:18:39 +0400

    Переделан дизайн, форма поиска сделана всплывающей

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!--******************PHP****************************-->
 <?php
 session_start();
 include "config.php"; 
@@ -125,7 +126,7 @@ function search($srch_collect, $srch_sql)
 	$page = intval($page);
 	if(empty($page) or $page < 0) $page = 1;
 		if($page > $total) $page = $total;
-//	echo 'num='.$num.'<br>';
+	echo 'num='.$num.'<br>';
 //	echo 'page='.$page.'<br>';
 //	echo 'start='.$start.'<br>';
 	$start = $page * $num - $num;
@@ -139,154 +140,229 @@ function search($srch_collect, $srch_sql)
 	else
 		$qr_order_by = " ORDER BY order_num";
 		
-	echo $_SERVER['REQUEST_URI'].'<br>';
-	echo $query.$qr_order_by.$qr_limit;
+//	echo $_SERVER['REQUEST_URI'].'<br>';
+//	echo $query.$qr_order_by.$qr_limit;
 	$result = mysql_query($query.$qr_order_by.$qr_limit) or die('query has dont work: ' . mysql_error());
 	while ($line[] = mysql_fetch_array($result, MYSQL_ASSOC))
 	
 ?>
 
+<!--******************HTML****************************-->
 
 <html>
 <head>
+	<script type="text/javascript" src="/jq/jquery.js"></script>
+	<link href="/css/style.css" rel="stylesheet" type="text/css"/>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
-	<style>
-	table
-	{
-		border-collapse:collapse;
-	}
-	table,th, td
-	{
-		border: 1px solid black;
-	}
-	</style>
 </head>
+
 <body>
-	<pre>
-	<form name = "search" action = "/index.php" method = "get">
-		Форма поиска: <br>
-		Ремонт:		<input type = "text" name = "order_num" value = "№ заказ-наряда"  onclick="value=''">; <input type = "text" name = "date" value = "Дата(гггг-мм-дд)" onclick="value=''">;<br>
-		Водитель:	<input type = "text" name = "name" value = "ФИО" onclick="value=''">; <input type = "text" name = "license_num" value = "№ Вод.удостоверения" onclick="value=''">;<br>
-		Бригада: 	<input type = "text" name = "gang_num" value = "№ Бригады" onclick="value=''">; <input type = "text" name = "specialization" value = "Специализация бригады" onclick="value=''">;<br>
-		Автомобиль: 	<input type = "text" name = "model" value = "Марка(модель)" onclick="value=''">; <input type = "text" name = "state_number" value = "Гос.номер" onclick="value=''">; <input type = "text" name = "VIN" value = "VIN" onclick="value=''">;<br>
-		<input type = "submit" name = "search_button" value = "Поиск"></form></pre>
-	<form name = "search_clr" action = "/index.php" method = "get">
-		<pre>
-		<input type = "submit" name = "clearbut" value = "Сбросить результаты поиска">
-		</pre>
-	</form>
+	
+	<div id="Header">
+		<h2>Список ремонтов</h2>
+	</div>
+	
+	<div id="Menu">
+		<div id="Menu_search">
+			<button>Форма поиска</button>
+		</div>
+		<form name = "add_repair" action = "/add_repair.php" method = "get">
+			<input type = "submit" name = "add_repair" value = "Добавить ремонт">
+		</form>
 		
-	
-	
-	<form name = "add_repair" action = "/add_repair.php" method = "get">
 		
-	Добавить ремонт:	<input type = "submit" name = "add_repair" value = "Добавить">
-	</form>
-	<form name = "order_by" action = "/index.php" method = "get">
-	<pre>
+	</div>
 	
-<?php 
-	if(count($line1) < $num)
-		$end = count($line)-1;
-	else
-		$end = $num;
-	if($total > 0)
-	{
-		if(isset($_GET[order_by]))
+	<div id="SrchForm" >
+		
+		<form name = "search" action = "/index.php" method = "get">
+		<table name="tbl">
+			<tr>
+				<td> 
+					Ремонт:	
+				</td>
+				<td>
+					<input type = "text" name = "order_num" value = "№ заказ-наряда"  onclick="value=''">;
+				</td>
+				<td>
+					<input type = "text" name = "date" value = "Дата(гггг-мм-дд)" onclick="value=''">;
+				</td>
+				<td rowspan="3">
+				<div align="center" id="Button"> 
+						
+						<form name = "search_clr" action = "/index.php" method = "get">				
+							<input type = "submit" name = "clearbut" value = "Сбросить">			
+						</form>
+							<input type = "submit" name = "search_button" value = "Поиск">
+						</div>
+				</td>
+			</tr>
+			<tr>
+				<td> 
+					Водитель:	
+				</td>
+				<td>
+					<input type = "text" name = "name" value = "ФИО" onclick="value=''">;
+				</td>
+				<td>
+					<input type = "text" name = "license_num" value = "№ Вод.удостоверения" onclick="value=''">;
+				</td>
+			</tr>
+			<tr>
+				<td> 
+					Бригада:	
+				</td>
+				<td>
+					<input type = "text" name = "gang_num" value = "№ Бригады" onclick="value=''">;
+				</td>
+				<td>
+					<input type = "text" name = "specialization" value = "Специализация бригады" onclick="value=''">;
+				</td>
+			</tr>
+				<td> 
+					Автомобиль:	
+				</td>
+				<td>
+					<input type = "text" name = "model" value = "Марка(модель)" onclick="value=''">;
+				</td>
+				<td>
+					<input type = "text" name = "state_number" value = "Гос.номер" onclick="value=''">;
+				</td>
+				<td>
+					<input type = "text" name = "VIN" value = "VIN" onclick="value=''">;
+				</td>
+			<tr>
+
+			</tr>
+		</table>
+			<!--Форма поиска: <br> -->
+		</form>
+	</div>
+	
+	<div id="Body">
+		
+		<form name = "order_by" action = "/index.php" method = "get">
+		<?php 
+			$end = count($line)-1;
+		if($total > 0)
 		{
-			$order_by = $_GET[order_by];
-			$_SESSION['order_by'] = $order_by;
-		}
-		elseif(isset($_SESSION['order_by']))
-			$order_by = $_SESSION['order_by'];
+			if(isset($_GET[order_by]))
+			{
+				$order_by = $_GET[order_by];
+				$_SESSION['order_by'] = $order_by;
+			}
+			elseif(isset($_SESSION['order_by']))
+				$order_by = $_SESSION['order_by'];
 			else
 				$order_by = "order_num";
-		echo "Сортировать по:     <select name = 'order_by' onchange='this.form.submit()'>";
-				if($order_by == "order_num" )
-						echo "<option value = 'order_num' selected>№ Заказ-наряда";
-					else
-						echo "<option value = 'order_num'>№ Заказ-наряда";
-					if($order_by == "date" )
-						echo "<option value = 'date' selected>Дате";
-					else
-						echo "<option value = 'date'>Дате";
-					if($order_by == "name" )
-						echo "<option value = 'name' selected>Имени водителя";
-					else
-						echo "<option value = 'name'>Имени водителя";
-					if($order_by == "license_num" )
-						echo "<option value = 'license_num' selected>№ вод.удостоверения";
-					else
-						echo "<option value = 'license_num'>№ вод.удостоверения";
-					if($order_by == "gang_num" )
-						echo "<option value = 'gang_num' selected>№ бригады";
-					else
-						echo "<option value = 'gang_num'>№ бригады";
-					if($order_by == "specialization" )
-						echo "<option value = 'specialization' selected>Специализации";
-					else
-						echo "<option value = 'specialization'>Специализации";
-					if($order_by == "model" )
-						echo "<option value = 'model' selected>Модели автомобиля";
-					else
-						echo "<option value = 'model'>Модели автомобиля";
-					if($order_by == "state_number" )
-						echo "<option value = 'state_number' selected>Гос.номеру";
-					else
-						echo "<option value = 'state_number'>Гос.номеру";
-					if($order_by == "VIN" )
-						echo "<option value = 'VIN' selected>VIN";
-					else
-						echo "<option value = 'VIN'>VIN";
-		echo" 	</select>
-				</pre>
-				</form>";
-		echo "<table >\n";
-		echo "	<tr>\n	
-				<th>№ Заказ-наряда</th>
-				<th>Дата</th>
-				<th>Фио водителя</th>
-				<th>№ Вод. удостоверения</th>
-				<th>№ Бригады</th>
-				<th>Специализация</th>
-				<th>Модель автомобиля</th>
-				<th>Гос.номер</th>
-				<th>VIN</th>
-				<th>Кнопка</th>
-			</tr>\n";
-	
-    	echo "\t<tr>\n";
+			echo "Сортировать по:     <select name = 'order_by' onchange='this.form.submit()'>";
+					if($order_by == "order_num" )
+							echo "<option value = 'order_num' selected>№ Заказ-наряда";
+						else
+							echo "<option value = 'order_num'>№ Заказ-наряда";
+						if($order_by == "date" )
+							echo "<option value = 'date' selected>Дате";
+						else
+							echo "<option value = 'date'>Дате";
+						if($order_by == "name" )
+							echo "<option value = 'name' selected>Имени водителя";
+						else
+							echo "<option value = 'name'>Имени водителя";
+						if($order_by == "license_num" )
+							echo "<option value = 'license_num' selected>№ вод.удостоверения";
+						else
+							echo "<option value = 'license_num'>№ вод.удостоверения";
+						if($order_by == "gang_num" )
+							echo "<option value = 'gang_num' selected>№ бригады";
+						else
+							echo "<option value = 'gang_num'>№ бригады";
+						if($order_by == "specialization" )
+							echo "<option value = 'specialization' selected>Специализации";
+						else
+							echo "<option value = 'specialization'>Специализации";
+						if($order_by == "model" )
+							echo "<option value = 'model' selected>Модели автомобиля";
+						else
+							echo "<option value = 'model'>Модели автомобиля";
+						if($order_by == "state_number" )
+							echo "<option value = 'state_number' selected>Гос.номеру";
+						else
+							echo "<option value = 'state_number'>Гос.номеру";
+						if($order_by == "VIN" )
+							echo "<option value = 'VIN' selected>VIN";
+						else
+							echo "<option value = 'VIN'>VIN";
+			echo" 	</select>
+					</form>";
+			echo "<table >\n";
+			echo "	<tr>\n	
+					<th>№ Заказ-наряда</th>
+					<th>Дата</th>
+					<th>Фио водителя</th>
+					<th>№ Вод. удостоверения</th>
+					<th>№ Бригады</th>
+					<th>Специализация</th>
+					<th>Модель автомобиля</th>
+					<th>Гос.номер</th>
+					<th>VIN</th>
+				</tr>\n";
+		
+			echo "\t<tr>\n";
 
-		for( $i = 0;  $i <$end; $i++)
-		{
-			$delnum = $line[$i]['order_num'];
-			echo "
-				<tr>
-				<td>".$line[$i]['order_num']."</td>
-				<td>".$line[$i]['date']."</td>
-				<td>".$line[$i]['name']."</td>
-				<td>".$line[$i]['license_num']."</td>
-				<td>".$line[$i]['gang_num']."</td>
-				<td>".$line[$i]['specialization']."</td>
-				<td>".$line[$i]['model']."</td>
-				<td>".$line[$i]['state_number']."</td>
-				<td>".$line[$i]['VIN']."</td>
-				<td>
-				<form name = 'del' action = '/index.php' method = 'get'>
-						<input type='hidden' name='delnum' value='$delnum'> 
-						<input type = 'submit' width = 10 name = 'del' value = 'Удалить'>
-					</form>
-				</td>
-				<tr>
-			";
+			for( $i = 0;  $i <$end; $i++)
+			{
+				$delnum = $line[$i]['order_num'];
+				echo "
+					<tr>
+					<td>".$line[$i]['order_num']."</td>
+					<td>".$line[$i]['date']."</td>
+					<td>".$line[$i]['name']."</td>
+					<td>".$line[$i]['license_num']."</td>
+					<td>".$line[$i]['gang_num']."</td>
+					<td>".$line[$i]['specialization']."</td>
+					<td>".$line[$i]['model']."</td>
+					<td>".$line[$i]['state_number']."</td>
+					<td>".$line[$i]['VIN']."</td>
+					<td>
+						<form name = 'del' action = '/edit.php' method = 'get'>
+							<input type='hidden' name='order_num' value='$delnum'>
+							<input type='hidden' name='edit' value=''>  
+							<input type='image' src='/images/edit.png' alt='edit'>
+						</form>	
+						</td>
+						<td>
+						<form name = 'del' action = '/print.php' method = 'get'>
+							<input type='hidden' name='order_num' value='$delnum'>
+							<input type='image' src='/images/print.png' alt='pt'>
+						</form>	
+						</td>
+						<td>
+						<form name = 'del' action = '/index.php' method = 'get'>
+							<input type='hidden' name='delnum' value='$delnum'> 
+							<input type='hidden' name='del' value=''>  
+							<input type='image' src='/images/delete.png' alt='del'>
+						</form>
+						</td>
+					<tr>
+				";
+			}
+			echo "</table>";
+			echo '<center>';
+			navigate($pagename, $page, (int)$total);
+			echo '</center>';
 		}
-		echo "</table>";
-		echo '<center>';
-		navigate($pagename, $page, (int)$total);
-		echo '</center>';
-	}
-	else
-		echo "Нет ни одной записи.";
-?>
+		else
+			echo "Нет ни одной записи.";
+		?>
+		
+	</div>
+	
+<script>	
+$(function(){                       // после загрузки страницы
+   $('button').click(function(){// на нажатие h1 вешаем обработчик, который
+     $('#SrchForm').toggle('slow');        // у элемента с #toggler переключет видимость
+   });
+});
+</script>
 </body>
 </html>
