df4f670c5851c362aeedc6be2fb390288cf5bf84
ZeRuS04 <zexa05@yandex.ru>
2013-12-25 05:12:27 +0400

    Исправленна функция добавления. Добавлена финишная страница добавления файлов

--- add_repair.php
+++ add_repair.php
@@ -1,5 +1,5 @@
-
 <?php
+session_start();
 
 include "config.php"; 
 
@@ -9,276 +9,276 @@ $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'xtgjxtv';
 $dbname = 'db_auto';
-/*
-$query = "SELECT 	order_num, date, 
-					driver.name, repair.license_num, 
-					repair.gang_num, gang.specialization,
-					auto.model, auto.state_number, repair.VIN 
-			FROM repair
-				JOIN auto 		ON repair.VIN = auto.VIN
-				JOIN driver 	ON repair.license_num = driver.license_num
-				JOIN gang		ON repair.gang_num = gang.gang_num
-		";
 
-
-$srch_collect = array("№ заказ-наряда" => "order_num",
-						"Дата(гггг-мм-дд)" => "date",
-						"ФИО" => "name",
-						"№ Вод.удостоверения" => "license_num",
-						"№ Бригады" => "gang_num",
-						"Специализация бригады" => "specialization",
-		//				"Id приемщика" => "id_acceptor",
-		//				"ФИО приемщика" => "acceptor_name",
-						"Марка(модель)" => "model",
-						"Гос.номер" => "state_number",
-						"VIN" => "VIN");
-$srch_sql = array("order_num" => "order_num",
-						"date" => "date",
-						"name" => "driver.name",
-						"license_num" => "repair.license_num",
-						"gang_num" => "repair.gang_num",
-						"specialization" => "gang.specialization",
-						"model" => "auto.model",
-						"state_number" => "auto.state_number",
-						"VIN" => "repair.VIN");
-
-*/
     $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die ("Could not connect to host ...");	
     mysql_select_db($dbname) or die ("Could not select database student ...");
-/*
-	$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-	while ($line1[] = mysql_fetch_array($result, MYSQL_ASSOC))
-
-	
-	$page = intval($page);
-//	echo 'page='.$page.'<br>';
-//	echo 'total='.$total.'<br>';
-	if(empty($page) or $page < 0) $page = 1;
-		if($page > $total) $page = $total;
-//	echo 'num='.$num.'<br>';
-//	echo 'page='.$page.'<br>';
-//	echo 'start='.$start.'<br>';
-	$start = $page * $num - $num;
-	if($start < 0)
-		$start = 0;
-	$qr_limit = " LIMIT $start,$num";
-	if($_GET[group_by])
-		$qr_group = " GROUP BY ".$_GET[group_by];
-	else
-		$qr_group = "";
-		echo $_SERVER['REQUEST_URI'].'<br>';
-	echo $query.$qr_group.$qr_limit;
-	*/ 
-
+	if(isset($_SESSION['p_array']))
+	{
+		$p_array = $_SESSION['p_array'];
+		foreach($p_array as $a)
+		{
+			echo $a;
+		}
+	}
+	if($_GET[order_num])
+	{
+		$query_full = "INSERT INTO repair (order_num, organization, date, description,  license_num, gang_num, id_acceptor, VIN)  VALUES (";
+		$query_full .= "'".$_GET[order_num]."'," ;
+		$query_full .="'no', ";
+		$query_full .="'".$_GET[date]."',"  ;
+		$query_full .="'".$_GET[info]."',"  ;
+		$query_full .="'".$_GET[driver]."',"  ;
+		$query_full .="'".$_GET[gang]."',"  ;
+		$query_full .="'".$_GET[acc]."',"  ;
+		$query_full .="'".$_GET[auto]."')";
+		echo $query_full."<br>";
+	}
+	elseif($_SESSION[order_num])
+	{
+		$query_full = "INSERT INTO repair (order_num, organization, date, description,  license_num, gang_num, id_acceptor, VIN)  VALUES (";
+		$query_full .= "'".$_SESSION['order_num']."'," ;
+		$query_full .="'no', ";
+		$query_full .="'".$_SESSION['date']."',"  ;
+		$query_full .="'".$_SESSION['info']."',"  ;
+		$query_full .="'".$_SESSION['driver']."',"  ;
+		$query_full .="'".$_SESSION['gang']."',"  ;
+		$query_full .="'".$_SESSION['acc']."',"  ;
+		$query_full .="'".$_SESSION['auto']."')";
+		echo $query_full."<br>";
+	}
+/*	if(isset($_GET[next]))
+	{
+		echo "<META HTTP-EQUIV='Refresh' CONTENT='0; URL=add_repair_next.php?order_num=$_GET[order_num]&date=$_GET[date]&info=$_GET[info]&driver=$_GET[driver]&auto=$_GET[auto]&gang=$_GET[gang]&acc=$_GET[acc]' >";
+		//$result = mysql_query($query_full) or die('query has dont work: ' . mysql_error());
+	}*/
+	if(isset($_GET[add]))
+	{
+		echo "<META HTTP-EQUIV='Refresh' CONTENT='0; URL=add_repair_finish.php?order_num=$_GET[order_num]&date=$_GET[date]&info=$_GET[info]&driver=$_GET[driver]&auto=$_GET[auto]&gang=$_GET[gang]&acc=$_GET[acc]' >";
+		//$result = mysql_query($query_full) or die('query has dont work: ' . mysql_error());
+	}
+	if(isset($_GET[clear]))
+	{
+		unset($_SESSION['order_num']);
+		unset($_GET[order_num]);
+		unset($_SESSION['date']);
+		unset($_GET[date]);
+		unset($_SESSION['auto']);
+		unset($_GET[auto]);
+		unset($_SESSION['driver']);
+		unset($_GET[driver]);
+		unset($_SESSION['gang']);
+		unset($_GET[gang]);
+		unset($_SESSION['acc']);
+		unset($_GET[acc]);
+		unset($_SESSION['info']);
+		unset($_GET[info]);
+		unset($_SESSION['spares']);
+		unset($_GET[spares]);
+		unset($_SESSION['parts']);
+		unset($_GET[parts]);
+	}
 	
 ?>
 
+<!--******************HTML****************************-->
 
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
-	
-	<style>
-	html, body{
-margin:3;
-padding:10;
-}/* отступы и поля элементовhtmlиbody равны нулю*/
-#Link{width: 700px;}
-#Box{
-	background-color: #ffff00;
-	width: 700px;  /*width: 700  - ширинаблокаBoxравна 700 пикселей  */
-        border: 1px dotted; /* Пунктирнаярамка */
-	margin:0avto;}  /*margin:0  -отступы Блока Box сверху и снизу равны нулю*/   
-#SubBox{
-	height: 470px;
-	background-color: #000000;
-	margin:0avto;}  /*margin:0  -отступы Блока Box сверху и снизу равны нулю*/   
-
-#Sort {
-	
-	background-color: #ff9900;
-	float:left;
-	width:250px;
-	height: 100%;
-	}
-
-#Info {
-	background-color: #0099cc;
-	float:right;
-	width:450px;	
-	height: 100%;
-	}
-	
-#LeftSide {
-	
-	background-color: #ff9900;
-	float:left;
-	width:350px;
-	height: 100%;
-	}
-
-#RightSide{
-	background-color: #0099cc;
-	float:right;
-	width:350px;	
-	height: 100%;
-	}
-
-#Footer {
-	background-color: #e1e1e1;	
-	clear:left;	
-	}
-
-	table
-	{
-		border-collapse:collapse;
-	}
-	table,th, td
-	{
-		border: 1px solid black;
-	}
-	</style>
+	<script type="text/javascript" src="/jq/jquery.js"></script>
 	<link href="/css/style.css" rel="stylesheet" type="text/css"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
 </head>
-<body>
-<form name = "edit_and_view" action = "script/request.php" method = "get">
-<div id="Link"> 
-<a href="/index.php">Назад</a>
-</div>
 
-<div id="Box">
-<div id="SubBox">
-<!-- ====================LeftSide====================-->
-<div id="LeftSide">
-	Номер заказ-наряда:
-		<input type = "text" width = 10 name = "order_num" value = "">
+<body>
 	
-<br>
-	Дата:
-	<input type = "text" name = "date" value = "">	
-<br>
-	Автомобиль:
-	<?php  
-	$query ="SELECT model, VIN FROM auto";
-	$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-	while ($line[] = mysql_fetch_array($result, MYSQL_ASSOC))
-	?>
-	<select name = "auto">
-	<?php  	
-	foreach ($line as $l)
-	{
-	 echo "<option value = '$l[model]'>$l[model]";
-	 
-	}	
-		?>
-    </select>
-<br>
-	Водитель:
-	<?php  
-	$query ="SELECT name, license_num FROM driver";
-	$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-	while ($line2[] = mysql_fetch_array($result, MYSQL_ASSOC))
-	?>
-	<select name = "driver">
-	<?php  	
-	foreach ($line2 as $l)
-	{
-	 echo "<option value = '$l[license_num]'>$l[name]";
-	 
-	}	
-		?>
-    </select>
-<br>		
-	Бригада:
-	<?php  
-	$query ="SELECT specialization, gang_num FROM gang";
-	$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-	while ($line3[] = mysql_fetch_array($result, MYSQL_ASSOC))
-	?>
-	<select name = "gang">
-	<?php  	
-	foreach ($line3 as $l)
-	{
-	 echo "<option value = '$l[gang_num]'>$l[specialization]";
-	}	
-	?>
-    </select>	
-<br>
-	Приемщик: 
-	<?php  
-	$query ="SELECT name, id_acceptor FROM acceptor";
-	$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-	while ($line4[] = mysql_fetch_array($result, MYSQL_ASSOC))
-	?>
-	<select name = "acc">
-	<?php  	
-	foreach ($line4 as $l)
-	{
-	 echo "<option value = '$l[id_acceptor]'>$l[name]";
-	}	
-	?>
-    </select>	
-<br>	
-<br>
-	Запчасти:<br>
-	<select name = "parts" size=10>
+	<div id="Header">
+		<h2>Добавление ремонта:</h2>
+	</div>
+	<div id="Menu">
+		<div id="Menu_search"><button>Назад</button></div>
+		<form name = "add_rep" action = "/add_repair.php" method = "get"><input type = "submit" name ="clear" value = "Oчистить">
+		<input type = "submit" name ="add" value = "Добавить">
 	
-    </select>	
-	<br>
-		<?php  
-		$query ="SELECT name, parts_num FROM spares";
-		$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-		while ($line5[] = mysql_fetch_array($result, MYSQL_ASSOC))
-		?>
-		<select name = "spares">
-		<?php  	
-		foreach ($line5 as $l)
-		{
-		 echo "<option value = '$l[parts_num]'>$l[name]";
-		}	
-		?>	
-		</select>
-	<br>
-	<input type = "button" name = "add_pinlist" value = "Добавить запчасть в список">
-</div>
-<!-- ================================================-->
-<!-- ====================RightSide===================-->
-<div id="RightSide">
-	Описание:<br>
-	<textarea name = "info" rows = "10" cols = "40">
+	</div>
 	
-	</textarea>
-<br>
-	Работы:<br>
-	<select name = "works" size=10>
+	<div id="Body">
+		<div id="LeftSide">
+			<br>
+			Номер заказ-наряда:
+			<?php 
+			if(isset($_GET[order_num]))
+			{
+				echo "<input type = 'text' width = 10 name = 'order_num' value = '".$_GET[order_num]."'>";
+				$_SESSION['order_num'] = $_GET[order_num];
+			}
+			elseif(isset($_SESSION['order_num']))
+				echo "<input type = 'text' width = 10 name = 'order_num' value = '".$_SESSION['order_num']."'>";
+			else
+				echo "<input type = 'text' width = 10 name = 'order_num' value = ''>";
+			?>
+				
+			
+		<br>
+			Дата:
+			<?php 
+			if(isset($_GET[date]))
+			{
+				echo "<input type = 'text' width = 10 name = 'date' value = '".$_GET[date]."'>";
+				$_SESSION['date'] = $_GET[date];
+			}
+			elseif(isset($_SESSION['date']))
+				echo "<input type = 'text' width = 10 name = 'date' value = '".$_SESSION['date']."'>";
+			else
+				echo "<input type = 'text' width = 10 name = 'date' value = ''>";
+			?>
+		<br>
+			Автомобиль:
+			<?php  
+			$query ="SELECT model, VIN FROM auto";
+			$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
+			while ($line[] = mysql_fetch_array($result, MYSQL_ASSOC))
+			?>
+			<select name = "auto">
+			<?php  	
+			foreach ($line as $l)
+			{
+				
+				if(isset($_GET[auto]) && ($_GET['auto'] == $l[VIN]))
+				{
+					$_SESSION['auto'] = $_GET[auto];
+					echo "<option value = '$l[VIN]' selected>$l[model]";
+				}
+				elseif(isset($_SESSION['auto']) && ($_SESSION['auto'] == $l[VIN]))
+				{
+					echo "<option value = '$l[VIN]' selected>$l[model]";
+				}
+				else
+					echo "<option value = '$l[VIN]'>$l[model]";
+					
+			 
+			 
+			}	
+				?>
+			</select>
+		<br>
+			Водитель:
+			<?php  
+			$query ="SELECT name, license_num FROM driver";
+			$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
+			while ($line2[] = mysql_fetch_array($result, MYSQL_ASSOC))
+			?>
+			<select name = "driver">
+			<?php  	
+			foreach ($line2 as $l)
+			{
+				if(isset($_GET[driver]) && ($_GET['driver'] == $l[license_num]))
+				{
+					$_SESSION['driver'] = $_GET[driver];
+					echo "<option value = '$l[license_num]' selected>$l[name]";
+				}
+				elseif(isset($_SESSION['driver']) && ($_SESSION['driver'] == $l[license_num]))
+				{
+					echo "<option value = '$l[license_num]' selected>$l[name]";
+				}
+				else
+					echo "<option value = '$l[license_num]'>$l[name]";
+					
+			}	
+				?>
+			</select>
+		<br>		
+			Бригада:
+			<?php  
+			$query ="SELECT specialization, gang_num FROM gang";
+			$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
+			while ($line3[] = mysql_fetch_array($result, MYSQL_ASSOC))
+			?>
+			<select name = "gang">
+			<?php  	
+			foreach ($line3 as $l)
+			{		
+				if(isset($_GET[gang]) && ($_GET[gang] == $l[gang_num]))
+				{
+					$_SESSION['gang'] = $_GET[gang];
+					echo "<option value = '$l[gang_num]' selected>$l[gang_num]";
+				}
+				elseif(isset($_SESSION['gang']) && ($_SESSION['gang'] == $l[gang_num]))
+				{
+					echo "<option value = '$l[gang_num]' selected>$l[gang_num]";
+				}
+				else
+					echo "<option value = '$l[gang_num]'>$l[gang_num]";
+				
+			}	
+			?>
+			</select>	
+		<br>
+			Приемщик: 
+			<?php  
+			$query ="SELECT name, id_acceptor FROM acceptor";
+			$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
+			while ($line4[] = mysql_fetch_array($result, MYSQL_ASSOC))
+			?>
+			<select name = "acc">
+			<?php  	
+			foreach ($line4 as $l)
+			{
+				if(isset($_GET[acc]) && ($_GET['acc'] == $l[id_acceptor]))
+				{
+					$_SESSION['acc'] = $_GET[acc];
+					echo "<option value = '$l[id_acceptor]' selected>$l[name]";
+				}
+				elseif(isset($_SESSION['acc']) && ($_SESSION['acc'] == $l[id_acceptor]))
+				{
+					echo "<option value = '$l[id_acceptor]' selected>$l[name]";
+				}
+				else
+					echo "<option value = '$l[id_acceptor]'>$l[name]";
+
+			}	
+			?>
+			</select>	
+		<br>
+
+		</div>
+		<!-- ================================================-->
+		<!-- ====================RightSide===================-->
+		<div >
+			Описание:<br>
+			<textarea name = "info" rows = "10" cols = "40"><?php
+			if(isset($_GET[info]))
+			{
+				$_SESSION['info'] = $_GET[info];
+				echo $_GET[info];
+			}
+			elseif(isset($_SESSION['info']))
+				echo $_SESSION['info'];
+			  ?></textarea>
+		<br>
+
+		</div>
+		
+		
+		
+	</div>
 	
-    </select>	
-	<br>
-		<?php  
-		$query ="SELECT name, work_num FROM work";
-		$result = mysql_query($query) or die('query has dont work: ' . mysql_error());
-		while ($line6[] = mysql_fetch_array($result, MYSQL_ASSOC))
-		?>
-		<select name = "work_list">
-		<?php  	
-		foreach ($line6 as $l)
-		{
-		 echo "<option value = '$l[work_num]'>$l[name]";
-		}	
-		?>	
-		</select>
-	<br>
-	<input type = "button" name = "add_winlist" value = "Добавить работу в список">
-</div>
-</div>
-<!-- ================================================-->
-<!-- ==========Кнопка добавить запись================-->
-<div id="Footer">
-	<center>
-		<input type = "submit" value = "Добавить запись">
-	</center>	
-</div>
-<!-- ================================================-->
-</div>
+	
+<script>	
+$(function(){                       // после загрузки страницы
+   $('button').click(function(){// на нажатие h1 вешаем обработчик, который
+     window.location.replace("/index.php");
+       // у элемента с #toggler переключет видимость
+   });
+});
+</script>
 </body>
-</html>
+
+<script>	
+$(function(){                       // после загрузки страницы
+   $('button').click(function(){// на нажатие h1 вешаем обработчик, который
+     window.location.replace("/index.php");
+       // у элемента с #toggler переключет видимость
+   });
+});
+</script>
+</body>
